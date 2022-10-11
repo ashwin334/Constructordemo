b42e02895830e5b47583ee8842c00e04bf2cce12
ashwin333 <ashwinpatil832@gmail.com>
2022-10-11 10:51:32 +0530

    Add project files.

---
+++ Class2.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constructordemo
+{
+    public class  course
+    {
+        private int id, fees;
+        private string name;    
+        public course(int id ,int fees,string name)
+        {
+            this .id = id;
+            this.fees = fees;
+            this.name = name;
+        }
+       public string Print()
+       {
+           return $" {id}{name}{fees}"; 
+       }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constructordemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //course cs1 = new course(2,20000,"Net");
+            //Console,WriteLine(cs1.Print());
+
+            courseTostring css1 = new courseTostring(2, 2000, "Net");
+            Console.WriteLine( css1); 
+        }
+    }
+}
